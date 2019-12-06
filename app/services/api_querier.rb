37f0bdfb2462ab4af842be3fdf063de42069984f
Michael Cooper <macoop2363@gmail.com>
2019-12-06 15:06:49 -0700

    Made API service to query an api
    Co-authored-by: Christopher Kelling <51389815+cjkelling@users.noreply.github.com>

---
+++ app/services/api_querier.rb
@@ -0,0 +1,16 @@
+class ApiQuerier
+
+  def self.query_api(url, path, access_token)
+    response = connect(url, access_token).get(path)
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private 
+
+  def self.connect(url, access_token)
+    Faraday.new(url: url) do |faraday|
+      faraday.params["access_token"] = access_token
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
