7a1d2d5608e654e28d3acac037f09e9ebf4bac15
Michael Cooper <macoop2363@gmail.com>
2019-12-08 12:32:06 -0700

    Refactor test to use new Repository poro

--- spec/facades/user_dashboard_spec.rb
+++ spec/facades/user_dashboard_spec.rb
@@ -13,16 +13,19 @@ RSpec.describe UserDashboardFacade do
 
     expect(repos.length).to eq(30)
 
-    sample_repos = [
-      { "brownfield-of-dreams" => "https://github.com/cjkelling/brownfield-of-dreams" },
-      { "apartments_1908"      => "https://github.com/CoopTang/apartments_1908" },
-      { "apollo_14"            => "https://github.com/CoopTang/apollo_14" },
-      { "B2_mid_mod_1908"      => "https://github.com/CoopTang/B2_mid_mod_1908" },
-      { "battleship"           => "https://github.com/CoopTang/battleship" }
-    ]
-    
-    sample_repos.each do |repo|
-      expect(repos.include?(repo)).to eq(true)
-    end
+    expect(repos[0].name).to eq("B2_mid_mod_1908")
+    expect(repos[0].url).to eq("https://github.com/CoopTang/B2_mid_mod_1908")
+
+    expect(repos[1].name).to eq("FCT")
+    expect(repos[1].url).to eq("https://github.com/CoopTang/FCT")
+
+    expect(repos[2].name).to eq("apartments_1908")
+    expect(repos[2].url).to eq("https://github.com/CoopTang/apartments_1908")
+
+    expect(repos[3].name).to eq("apollo_14")
+    expect(repos[3].url).to eq("https://github.com/CoopTang/apollo_14")
+
+    expect(repos[4].name).to eq("battleship")
+    expect(repos[4].url).to eq("https://github.com/CoopTang/battleship")
   end
 end
