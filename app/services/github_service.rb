e0b7568281acadd56a96a7a98f3f7fcf24294bb7
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-10 14:30:42 -0700

    Add a git_user method to the GithubService
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,9 +1,9 @@
 class GithubService
-  URL = 'https://api.github.com/'
-  USER_PATH = 'user'
-  USER_REPOS_PATH = 'user/repos'
-  USER_FOLLOWERS_PATH = 'user/followers'
-  USER_FOLLOWING_PATH = 'user/following'
+  URL = 'https://api.github.com/'.freeze
+  USER_PATH = 'user'.freeze
+  USER_REPOS_PATH = 'user/repos'.freeze
+  USER_FOLLOWERS_PATH = 'user/followers'.freeze
+  USER_FOLLOWING_PATH = 'user/following'.freeze
 
   def self.get_repos(user_token)
     response = ApiQuerier.query_api(
@@ -12,6 +12,7 @@ class GithubService
       user_token
     )
   end
+
   def self.get_followers(user_token)
     response = ApiQuerier.query_api(
       URL,
@@ -19,10 +20,19 @@ class GithubService
       user_token
     )
   end
+
   def self.get_following(user_token)
     response = ApiQuerier.query_api(
       URL,
       USER_FOLLOWING_PATH,
+      user_token
+    )
+  end
+
+  def self.get_user(user_token, user_handle)
+    response = ApiQuerier.query_api(
+      URL,
+      "users/#{user_handle}",
       user_token
     )
   end
