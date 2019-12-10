3e7650ad8519f0cd80d35d48b860ad4f6585a2de
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-10 14:26:44 -0700

    Features test to verify that a user can email an invitation to a person by using their github handle.
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

---
+++ spec/features/user/emails/invitations_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'As a registered user' do
+  describe 'when I visit my dashboard' do
+    describe 'I should see a section to invite users from GitHub.' do
+      before :each do
+        user = create(:user, token: ENV['GITHUB_TESTING_TOKEN'])
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit dashboard_path
+
+        click_button 'Send an Invite'
+
+        expect(current_path).to eq(invite_new_path)
+      end
+
+      it 'I can invite a user that has an email address associated with their github account', :vcr do
+        fill_in 'github_handle', with: 'CoopTang'
+
+        click_button 'Send Invite'
+
+        expect(current_path).to eq(dashboard_path)
+
+        expect(page).to have_content('Successfully sent invite!')
+      end
+
+      it 'I get a notification if the user doesnt have an email address associated with their account', :vcr do
+        fill_in 'github_handle', with: 'cjkelling'
+
+        click_button 'Send Invite'
+
+        expect(current_path).to eq(dashboard_path)
+
+        expect(page).to have_content('The Github user you selected doesn\'t have an email address associated with their account.')
+      end
+    end
+  end
+end
+
+# ApiQuerier.query_api('https://api.github.com/', 'search/users?q=params[:github_handle]', ENV['GITHUB_TESTING_TOKEN'])
