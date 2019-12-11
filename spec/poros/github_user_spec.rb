1daa10228a1ce788b425214581054f3ef165a857
Christopher Kelling <cj.kelling88@gmail.com>
2019-12-11 13:49:04 -0700

    Update github user poros to gather email from followers and compare them with users in our system to decided if they are friends or can be added as friends.
    Co-Authored-By: Michael Cooper <macoop2363@gmail.com>

--- spec/poros/github_user_spec.rb
+++ spec/poros/github_user_spec.rb
@@ -3,13 +3,31 @@ require 'rails_helper'
 RSpec.describe GithubUser do
   it 'has attributes' do
     user_params = {
-      login: "username",
-      html_url: "https://github.com/CoopTang"
+      login: 'username',
+      html_url: 'https://github.com/CoopTang',
+      email: 'email@example.com'
     }
 
     user = GithubUser.new(user_params)
 
     expect(user.user_name).to eq(user_params[:login])
     expect(user.profile_url).to eq(user_params[:html_url])
+    expect(user.email).to eq(user_params[:email])
+  end
+
+  it '.in_system?' do
+    create(:user, email: 'email@example.com')
+    user_params = {
+      login: 'username',
+      html_url: 'https://github.com/CoopTang',
+      email: 'email@example.com'
+    }
+
+    user = GithubUser.new(user_params)
+
+    expect(user.in_system?).to eq(true)
+  end
+
+  it 'text' do
   end
 end
